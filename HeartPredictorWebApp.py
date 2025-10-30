db7c89f3ef9e384d4d09b7ca4f78c549950263e2
Anshul Kumar <anshulkumarchauhan25@gmail.com>
2025-10-30 09:49:08 +0530

    modified for deployment

--- HeartPredictorWebApp.py
+++ HeartPredictorWebApp.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model=pickle.load(open('C:\Users\Lenovo\Desktop\ML_task03\Heart_Disease_Prediction_Model/Trained_model.sav','rb'))
+loaded_model=pickle.load(open('C:\Users\Lenovo\Desktop\ML_task03\Heart_Disease_Prediction_Model\Trained_model.sav','rb'))
 
 #creating function for prediction
 def heart_prediction(input_data):
